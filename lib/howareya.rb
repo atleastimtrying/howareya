ce6ad98eb54eb088abd395327a3afa3a443cd6dc
Anders Fisher <anders@js-dev.co.uk>
2019-03-13 11:40:23 +0000

    basic httparty implementation

--- lib/howareya.rb
+++ lib/howareya.rb
@@ -1,5 +1,33 @@
 require "howareya/version"
 
 module Howareya
-  # Your code goes here...
+  def self.setup(url, api_key)
+    @api_key = api_key
+    @url = url
+  end
+
+  def self.details
+    { api_key: @api_key, url: @url}
+  end
+
+  def self.record_metric key, value
+    response = HTTParty.post(@url, :body => { :api_key => @api_key, 
+                             :key => key, 
+                             :value => value })
+    raise HowareyaMissingMetricError if response.code === 404
+    raise HowareyaBadAPIKeyError if response.code === 401
+  end
+
+end
+
+class HowareyaMissingMetricError < StandardError
+  def initialize(msg="It looks like you havent registered a metric at Howareya")
+    super
+  end
+end
+
+class HowareyaBadAPIKeyError < StandardError
+  def initialize(msg="It looks like you're using an invalid api key for Howareya")
+    super
+  end
 end
--- spec/howareya_spec.rb
+++ spec/howareya_spec.rb
@@ -1,11 +1,44 @@
 require "spec_helper"
+require "httparty"
 
 RSpec.describe Howareya do
+  let(:url){"http://www.example.com/howareya/record_metric"}
+  let(:api_key){"howareya api key"}
+
   it "has a version number" do
     expect(Howareya::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "can be setup with a url and an API key" do
+    Howareya.setup(url, api_key)
+    expect(Howareya.details).to eq({ api_key: api_key, url: url})
+  end
+
+  context "when setup" do
+    before { Howareya.setup(url, api_key) }
+
+    it "calls out to httparty" do
+      expect(HTTParty).to receive(:post) { double :response, code: 200 }
+      Howareya.record_metric("Foo", 123)
+    end
+
+    context "with a bad api key"  do
+      before { allow(HTTParty).to receive(:post) { double :response, code: 401 } }
+      it "raises an error" do
+        expect {
+          Howareya.record_metric("Foo", 123)
+        }.to raise_error(HowareyaBadAPIKeyError)
+      end
+    end
+
+    context "with a bad metric key"  do
+      before { allow(HTTParty).to receive(:post) { double :response, code: 404 } }
+      it "raises an error" do
+        expect {
+          Howareya.record_metric("Foo", 123)
+        }.to raise_error(HowareyaMissingMetricError)
+      end
+    end
+
   end
 end
